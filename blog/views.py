9b525e22449f8bbe4199aed6818a1122199066ba
Kirubaa-Nandhini <kirubaanandhini@testpress.in>
2026-01-27 13:15:50 +0530

    Add share blogs through email feature

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from django.views.generic import ListView
-
+from django.core.mail import send_mail
+from .forms import EmailPostForm
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
@@ -10,6 +11,8 @@ class PostListView(ListView):
     template_name = 'blog/post/list.html'
 
 
+
+from django.shortcuts import get_object_or_404, render
 
 def post_detail(request, year, month, day, slug):
     post = get_object_or_404(
@@ -20,10 +23,54 @@ def post_detail(request, year, month, day, slug):
         publish__month=month,
         publish__day=day
     )
+
     return render(
         request,
         'blog/post/detail.html',
-        {'post': post,
-         'page':page,
-         }
+        {
+            'post': post,
+        }
+    )
+
+def post_share(request,post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+
+        if form.is_valid():
+            cd = form.cleaned_data
+            to_email=cd['to']
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+
+            subject = f"{cd['name']} recommends you read {post.title}"
+
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}'s comments: {cd['comments']}"
+            )
+
+            send_mail(
+                subject,
+                message,
+                'admin@myblog.com',
+                [cd['to']]
+            )
+
+            sent = True
+    else:
+        form = EmailPostForm()
+
+    return render(
+        request,
+        'blog/post/share.html',
+        {
+            'post': post,
+            'form': form,
+            'sent': sent,
+            'to_email':to_email,
+        }
     )

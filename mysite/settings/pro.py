610fe14dec219f2ec8216c8e5ce9262320726297
Kirubaa-Nandhini <kirubaanandhini@testpress.in>
2026-02-16 16:56:37 +0530

    Configure production database

--- mysite/settings/pro.py
+++ mysite/settings/pro.py
@@ -1,20 +1,26 @@
+import os
+import dj_database_url
 from .base import *
+
 DEBUG = False
+
 ADMINS = (
- ('testpress', 'kirubaanandhini@testpress.in'),
+    ('testpress', 'kirubaanandhini@testpress.in'),
 )
-ALLOWED_HOSTS = ['*']
+
+ALLOWED_HOSTS = ['.onrender.com']
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mysite',
-        'USER': 'blogs',
-        'PASSWORD': 'testpress',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600
+    )
 }
+
 SECURE_SSL_REDIRECT = False
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
-
